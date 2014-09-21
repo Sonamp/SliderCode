e3496f719aae039584076538056ca3f82dad30b6
Sonamp <sonam.pahariya@gmail.com>
2014-09-21 01:44:08 -0700

    Second Attempt without colorbox

---
+++ AlternativeSliderapp/scripts/slideController.js
@@ -0,0 +1,34 @@
+﻿
+app.controller('slideController', function ($scope, $http) {
+    $http.get("scripts/exercise.json").success(function (data) {
+        $scope.response = data;        
+        $scope.images = data.attachment;
+        $scope.total = $scope.images.length;
+        
+    });
+   
+    // initial image index
+    $scope._Index = 0;
+
+    // if a current image is the same as requested image
+    $scope.isActive = function (index) {
+        return $scope._Index == index;
+    };
+
+    // show prev image
+    $scope.showPrev = function () {
+        $scope._Index = ($scope._Index > 0) ? --$scope._Index : $scope.images.length - 1;
+    };
+
+    // show next image
+    $scope.showNext = function () {
+        $scope._Index = ($scope._Index < $scope.images.length - 1) ? ++$scope._Index : 0;
+    };
+
+    // show a certain image
+    $scope.showPhoto = function (index) {
+        $scope._Index = index;
+    };
+});
+
+
